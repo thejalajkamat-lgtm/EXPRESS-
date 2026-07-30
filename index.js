9a626cb91c234e732a7df9ff9e7d1b368bad6c55
Jalaj Kamat <thejalajkamat@gmail.com>
2026-07-30 23:11:27 +0530

    sending response

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ app.listen(port, () => {
     console.log(`app is listening on port ${port}`)
 })
 app.use((req, res) => {
+    // console.log(req);
     console.log("request received")
+    // res.send("this is a basic response");
+    // res.send({
+    //     name: "apple",
+    //     color: "red",
+    // })
+   let code  = "<h1>Fruits<h1><ul><li>apple</li><li>orange</li><ul>";
+   res.send(code);
 })
