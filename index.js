548bc56aa73fae7ec06bcf4d61ef614228973a4a
Jalaj Kamat <thejalajkamat@gmail.com>
2026-08-03 20:53:36 +0530

    modified comment

--- index.js
+++ index.js
@@ -6,14 +6,29 @@ let port = 3000;
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`)
 })
-app.use((req, res) => {
-    // console.log(req);
-    console.log("request received")
-    // res.send("this is a basic response");
-    res.send({
-        name: "apple",
-        color: "red",
-    })
-   let code  = "<h1>Fruits<h1><ul><li>apple</li><li>orange</li><ul>";
-//    res.send(code);
+// app.use((req, res) => {
+//     // console.log(req);
+//     console.log("request received")
+//     // res.send("this is a basic response");
+//     res.send({
+//         name: "apple",
+//         color: "red",
+//     })
+//    let code  = "<h1>Fruits<h1><ul><li>apple</li><li>orange</li><ul>";
+// //    res.send(code);
+// })
+
+app.get("/hoe-1",(req,res)=> {
+    res.send("you contacted hoe1 🤏")
+})
+app.get("/hoe-2",(req,res)=> {
+    res.send("you contacted hoe2 🤏")
+})
+
+// app.get("*",(req, res) => {
+//     res.send("You contacted a hoe")
+// })
+
+app.post("/", (req,res)=> {
+    res.send("you sent a post request to root")
 })
