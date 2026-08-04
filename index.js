6183950a608168ad29efdd1df12ae4306444b794
Jalaj Kamat <thejalajkamat@gmail.com>
2026-08-04 20:06:56 +0530

    changes port number

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // console.dir(app);
-let port = 3000;
+let port = 8080;
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`)
 })
