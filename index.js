ec2da0e463fc22b739aba5a431c2c7041931d84f
Jalaj Kamat <thejalajkamat@gmail.com>
2026-08-04 22:34:21 +0530

    changed from hoe to person

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.listen(port, () => {
     console.log(`app is listening on port ${port}`)
 })
 // app.use((req, res) => {
-//     // console.log(req);
+//     console.log(req);
 //     console.log("request received")
-//     // res.send("this is a basic response");
+//     res.send("this is a basic response");
 //     res.send({
 //         name: "apple",
 //         color: "red",
@@ -17,18 +17,16 @@ app.listen(port, () => {
 //    let code  = "<h1>Fruits<h1><ul><li>apple</li><li>orange</li><ul>";
 // //    res.send(code);
 // })
-
-app.get("/hoe-1",(req,res)=> {
-    res.send("you contacted hoe1 🤏")
+app.get("/person1",(req,res)=> {
+    res.send("you contacted person1 🤏")
 })
-app.get("/hoe-2",(req,res)=> {
-    res.send("you contacted hoe2 🤏")
+app.get("/person2",(req,res)=> {
+    res.send("you contacted person2 🤏")
+})
+app.get("*",(req, res) => {
+    res.send("You contacted a wrong person 🤏")
 })
 
-// app.get("*",(req, res) => {
-//     res.send("You contacted a hoe")
+// app.post("/", (req,res)=> {
+//     res.send("you sent a post request to root")
 // })
-
-app.post("/", (req,res)=> {
-    res.send("you sent a post request to root")
-})
